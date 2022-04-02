8018492eeee9ce2d61bd70da7818692692dc375b
Joschka Hüllmann <joschka.huellmann@uni-muenster.de>
2022-04-02 12:42:27 +0200

    updated version number, support for thunderbird 91.*, open outlook webtabs

--- background.js
+++ background.js
@@ -1,9 +1,15 @@
 function openGoogleTabs() {
   browser.tabs.create({
-    url: "https://calendar.google.com/calendar/r"
+    url: "https://calendar.google.com/calendar/u/0/r/week"
   });
   browser.tabs.create({
     url: "https://keep.google.com/u/0/"
+  });
+  browser.tabs.create({
+    url: "https://outlook.office.com/calendar/view/week"
+  });
+  browser.tabs.create({
+    url: "https://outlook.office.com/mail/"
   });
 }
 
